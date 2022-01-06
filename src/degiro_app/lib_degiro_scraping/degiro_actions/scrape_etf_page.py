c8fca94863280fd40bcb917bd16d626d6bf6d35b
Robert Hommes <roberthommes@Roberts-MacBook-Pro.local>
2022-01-06 20:26:56 +0100

    FIX: Downgraded selenium and updated scrape logic

--- src/degiro_app/lib_degiro_scraping/degiro_actions/scrape_etf_page.py
+++ src/degiro_app/lib_degiro_scraping/degiro_actions/scrape_etf_page.py
@@ -1,5 +1,9 @@
+from typing import List, Tuple
+
 import attr
 from degiro_app.lib_degiro_scraping.degiro_actions.login_to_degiro import LoginToDeGiro
+from degiro_app.lib_degiro_scraping.degiro_pages.etf_page import EtfPage
+from pandas import DataFrame
 from selenium.webdriver.chrome.webdriver import WebDriver
 
 
@@ -7,12 +11,11 @@ from selenium.webdriver.chrome.webdriver import WebDriver
 class ScrapeEtfPage:
     driver: WebDriver = attr.ib()
 
-    def run(self) -> WebDriver:
+    def run(self) -> Tuple[WebDriver, List[DataFrame]]:
         login_to_degiro = LoginToDeGiro(driver=self.driver)
         driver = login_to_degiro.run()
 
         etf_page = EtfPage(driver=driver)
         etf_page.get_page()
-        etf_page.next_page_etf_table()
-        etf_page.next_page_etf_table()
-        return etf_page.driver
+        etf_page.scrape_tables()
+        return etf_page.driver, etf_page.tables
--- src/degiro_app/lib_degiro_scraping/degiro_pages/etf_page.py
+++ src/degiro_app/lib_degiro_scraping/degiro_pages/etf_page.py
@@ -1,6 +1,8 @@
 import time
+from typing import List
 
 import attr
+import pandas as pd
 from degiro_app.lib_degiro_scraping.degiro_pages.page_locators import EtfPageLocators
 from selenium.common.exceptions import ElementClickInterceptedException
 from selenium.webdriver.chrome.webdriver import WebDriver
@@ -10,14 +12,29 @@ from selenium.webdriver.common.by import By
 @attr.s
 class EtfPage:
     driver: WebDriver = attr.ib()
+    tables: List[pd.DataFrame] = attr.ib(default=[])
 
     def get_page(self) -> None:
         time.sleep(2)
         self.driver.get(url=EtfPageLocators.PAGE_URL.value)
 
-    def next_page_etf_table(self) -> None:
+    def scrape_table(self) -> None:
+        time.sleep(2)
+        dfs = pd.read_html(self.driver.page_source)
+        df = dfs[0]
+        df.replace(u'\xa0', u'', regex=True, inplace=True)
+        self.tables.append(df)
+
+    def next_page_etf_table(self) -> bool:
         time.sleep(2)
         try:
             self.driver.find_element(by=By.XPATH, value=EtfPageLocators.NEXT_PAGE_BUTTON_XPATH.value).click()
+            return True
         except ElementClickInterceptedException:
-            pass
+            return False
+
+    def scrape_tables(self) -> None:
+        scrape_switch = True
+        while scrape_switch:
+            self.scrape_table()
+            scrape_switch = self.next_page_etf_table()
