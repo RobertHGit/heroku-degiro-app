1c3dbd677d47d246c420d152fcd9e904c669f7b7
robert <roberthommes@gmail.com>
2022-02-02 22:36:04 +0100

    FIX: STDOUT logging enabled

--- src/degiro_app/run_app.py
+++ src/degiro_app/run_app.py
@@ -1,10 +1,13 @@
-import os
-
 import logging
+import os
+import sys
+
 from apscheduler.schedulers.blocking import BlockingScheduler
 from degiro_app.lib_degiro_scraping.degiro_actions.login_to_degiro import LoginToDeGiro
 from degiro_app.lib_degiro_scraping.degiro_actions.scrape_etf_page import ScrapeEtfPage
-from degiro_app.lib_degiro_scraping.degiro_pages.page_locators import etf_asset_allocation_search_urls
+from degiro_app.lib_degiro_scraping.degiro_pages.page_locators import (
+    etf_asset_allocation_search_urls,
+)
 from degiro_app.lib_degiro_scraping.utils.chrome_utils import (
     get_headless_chrome_options,
 )
@@ -14,11 +17,13 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from sqlalchemy.types import INTEGER, VARCHAR, FLOAT
 
-logging.basicConfig(
-    filename="newfile.log", format="%(asctime)s %(message)s", filemode="w"
-)
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+logger = logging.getLogger(__name__)
+logger.setLevel(level=os.environ.get("LOGLEVEL", "INFO"))
+
+streamHandler = logging.StreamHandler(sys.stdout)
+streamHandler.setFormatter(logging.Formatter("%(levelname)s - %(message)s"))
+logger.addHandler(streamHandler)
+
 
 scheduler = BlockingScheduler(timezone=timezone(zone="Europe/Amsterdam"))
 
@@ -27,21 +32,21 @@ scheduler = BlockingScheduler(timezone=timezone(zone="Europe/Amsterdam"))
 def run_scrape():
     """Daily job to scrape data and store."""
 
-    print("*** running daily task ***")
+    logger.info("running daily task")
     service = Service(executable_path=os.environ.get("CHROMEDRIVER_PATH"))
     driver = webdriver.Chrome(service=service, options=get_headless_chrome_options())
 
-    print("*** login to DeGiro ***")
+    logger.info("login to DeGiro")
     login_to_degiro = LoginToDeGiro(driver=driver)
     driver = login_to_degiro.run()
 
     for asset_class in etf_asset_allocation_search_urls.keys():
-        print(f"*** scraping etf_page | {asset_class} ***")
+        logger.info(f"scraping etf_page | {asset_class}")
 
         scrape_etf_page = ScrapeEtfPage(driver=driver, etf_asset_allocation=asset_class)
         driver, asset_df = scrape_etf_page.run()
 
-        print(f"*** writing results | df_shape: {asset_df.shape} ***")
+        logger.info(f"writing results | df_shape: {asset_df.shape}")
         asset_df.to_sql(
             name=f"etf_{asset_class}_data",
             con=get_db_engine(),
